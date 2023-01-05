19ee91eeba938ec5b93880d2ec94552bae19cc8f
hftjutyir <thomas.kodehode@gmail.com>
2023-01-05 12:59:02 +0100

    /feat added more projects

--- src/components/SectionProjects/SectionProjects.js
+++ src/components/SectionProjects/SectionProjects.js
@@ -3,8 +3,10 @@ import React, { useContext } from "react";
 import { DarkModeContext } from "../../contextState/DarkModeContext";
 
 // Images
-import project1 from "./ProjectsImages/albertWeskerQuotes.png";
-import project2 from "./ProjectsImages/paulAllanCard.png";
+import project1 from "./ProjectsImages/countdownToChristmas.png";
+import project2 from "./ProjectsImages/fakeWebstore.png";
+import project3 from "./ProjectsImages/albertWeskerQuotes.png";
+import project4 from "./ProjectsImages/paulAllanCard.png";
 
 // Styled components
 import {
@@ -59,24 +61,46 @@ export const SimpleSlider = () => {
     <StyledDiv light={!darkMode ? true : false}>
       <StyledSlider light={!darkMode ? true : false} {...settings}>
         <div name="projects">
-          <StyledH3>Albert Wesker quotes</StyledH3>
+          <StyledH3>Countdown to Christmas</StyledH3>
           <a
-            href="https://github.com/hftjutyir/Albert-Wesker"
+            href="https://thomasr1997.github.io/christmas-countdown/"
             target={"_blank"}
             rel={"noreferrer"}
           >
-            <StyledImg src={project1} alt="Albert Wesker quotes project" />
+            <StyledImg src={project1} alt="Countdown to Christmas project" />
           </a>
         </div>
 
-        <div>
-          <StyledH3>Paul Allan's card</StyledH3>
+        <div name="projects">
+          <StyledH3>Fake webstore</StyledH3>
           <a
-            href="https://github.com/hftjutyir/Paul-Allen-s-card"
+            href="https://thomasr1997.github.io/fake-webstore/#/men-clothing-page"
             target={"_blank"}
             rel={"noreferrer"}
           >
-            <StyledImg src={project2} alt="Paul Allan's card project" />
+            <StyledImg src={project2} alt="Fake webstore project" />
+          </a>
+        </div>
+
+        <div name="projects">
+          <StyledH3>Albert Wesker quotes</StyledH3>
+          <a
+            href="https://thomasr1997.github.io/Albert-Wesker/"
+            target={"_blank"}
+            rel={"noreferrer"}
+          >
+            <StyledImg src={project3} alt="Albert Wesker quotes project" />
+          </a>
+        </div>
+
+        <div name="projects">
+          <StyledH3>Paul Allan's card</StyledH3>
+          <a
+            href="https://thomasr1997.github.io/Paul-Allen-s-card/"
+            target={"_blank"}
+            rel={"noreferrer"}
+          >
+            <StyledImg src={project4} alt="Paul Allan's card project" />
           </a>
         </div>
       </StyledSlider>
